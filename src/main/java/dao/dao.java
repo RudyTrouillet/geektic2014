5edd71aee2fea92f5abf188ffc9050658f5924cd
unknown <rudy.trouillet@gmail.com>
2015-06-24 09:06:53 +0200

    Mise en place user & DAO

---
+++ src/main/java/dao/dao.java
@@ -0,0 +1,52 @@
+package dao;
+
+import user.user;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Repository
+public class dao {
+	@PersistenceContext
+    private EntityManager entityManager;
+
+    public dao() {}
+
+    public List<user> findAll(){
+        String query = "SELECT * FROM geek";
+        return entityManager.createQuery(query, user.class).getResultList();
+    }
+
+    public List<user> findById(Long id){
+        String query = "SELECT * FROM geek WHERE geek.id_geek = :id";
+
+        return entityManager.createQuery(query, user.class)
+                .setParameter("id", id)
+                .getResultList();
+    }
+
+    public List<user> findBySex(String sexe){
+        if(sexe.equals("")) return new ArrayList<user>();
+
+        String query = "SELECT * FROM geek WHERE geek.sexe = :sexe";
+
+        return entityManager.createQuery(query, user.class).setParameter("sexe", sexe).getResultList();
+    }
+
+    public List<user> findByInterest(List<String> interests){
+        if(interests.isEmpty()) return new ArrayList<user>();
+
+        String query = "SELECT * FROM geek JOIN geek_interest.id_interest WHERE geekInterest.id_interest=:id_interest";
+
+        return entityManager.createQuery(query, user.class)
+                .setParameter("id_interest", interests)
+                .getResultList();
+    }
+
+   
+}
---
+++ src/main/java/user/user.java
@@ -0,0 +1,58 @@
+package user;
+
+import javax.persistence.*;
+
+
+@Entity(name="User")
+@SequenceGenerator(name="USER_SEQ", sequenceName="user_seq")
+public class user {
+
+    @ManyToMany
+    @JoinTable(name = "GEEKTIC",
+            joinColumns = @JoinColumn(name = "id_user"),
+            inverseJoinColumns = @JoinColumn(name = "id_interest"))
+   
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="USER_SEQ")
+    private Long id_geek;
+    private String nom;
+    private String prenom;
+    private String mail;
+    private String sexe;
+
+    public user(Long id_geek, String nom, String prenom, String mail, String sexe) {
+        this.id_geek = id_geek;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.mail = mail;
+        this.sexe = sexe;
+    }
+
+    public user() {
+        this.nom = "";
+        this.prenom = "";
+        this.mail = "";
+        this.sexe = "";
+    }
+
+    public Long getId() {
+        return id_geek;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getSexe() {
+        return sexe;
+    }
+}
